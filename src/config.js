e167f001df28f0a15591e00327529ae5e7e37cd1
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-01-27 13:37:41 +0800

    upload

---
+++ src/config.js
@@ -0,0 +1,57 @@
+const config = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['react', 'sonarjs', 'unicorn', '@typescript-eslint'],
+  rules: {
+    semi: [2, 'always'],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    // React
+    'react/react-in-jsx-scope': 0,
+    'react/button-has-type': 2,
+    'react/prop-types': 0,
+    'react/no-array-index-key': 2,
+    // Unicorn
+    'unicorn/prefer-module': 0,
+    'unicorn/no-useless-undefined': 0,
+    'unicorn/prevent-abbreviations': 0,
+    'unicorn/no-await-expression-member': 0,
+    // Typescript
+    '@typescript-eslint/no-misused-promises': 0,
+    '@typescript-eslint/no-unused-vars': [2],
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-namespace': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
+
+module.exports = config;
