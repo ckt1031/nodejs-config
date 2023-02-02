788f14fbbd85cddab51f03ffb3e4257bef237c91
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-02 16:35:23 +0800

    feat(eslint-config): new `typescript-strict`

---
+++ apps/eslint-config/typescript-strict.js
@@ -0,0 +1,30 @@
+const merge = require('deepmerge');
+const defaultConfig = require('./default');
+
+const config = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['deprecation', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'deprecation/deprecation': 1,
+    // Typescript
+    '@typescript-eslint/no-misused-promises': 0,
+    '@typescript-eslint/consistent-type-imports': 2,
+    '@typescript-eslint/consistent-type-exports': 2,
+    '@typescript-eslint/no-redundant-type-constituents': 2,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-namespace': 0,
+  },
+};
+
+module.exports = merge(defaultConfig, config);
