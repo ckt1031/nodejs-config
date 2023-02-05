89721dcbab041f20a484c4fd71cd45a2ab8c96c8
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-05 12:15:23 +0800

    feat(eslint-config): optimized build config

---
+++ apps/eslint-config/rollup.config.mjs
@@ -0,0 +1,81 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import rollupPluginCommonjs from '@rollup/plugin-commonjs';
+import rollupPluginNodeResolve from '@rollup/plugin-node-resolve';
+import rollupPluginTypescript from '@rollup/plugin-typescript';
+import rollupPluginAutoExternal from 'rollup-plugin-auto-external';
+
+const dir = './src/';
+const files = fs.readdirSync(dir).filter(file => file.endsWith('.ts'));
+
+const plugins = [
+  rollupPluginAutoExternal(),
+  rollupPluginNodeResolve(),
+  rollupPluginCommonjs(),
+  rollupPluginTypescript({
+    tsconfig: 'tsconfig.json',
+  }),
+];
+
+/**
+ * @typedef {import('rollup').RollupOptions} RollupOptions
+ * @typedef {import('rollup').OutputOptions} OutputOptions
+ * @typedef {import('rollup').Plugin} Plugin
+ */
+
+/**
+ * @typedef {Object} ParticularFormatConfig
+ * @property {string} input
+ * @property {OutputOptions} output
+ * @property {Array<Plugin>} plugins
+ */
+
+/**
+ * Generates the Rollup configuration for CJS (CommonJS) builds.
+ *
+ * @param {string} filename - The name of the input TypeScript file.
+ * @returns {ParticularFormatConfig} The Rollup configuration for CJS builds.
+ */
+function getCjsConfig(filename) {
+  return {
+    input: `${dir}${filename}`,
+    output: {
+      dir: './dist',
+      exports: 'default',
+      entryFileNames: `${path.basename(filename, '.ts')}.cjs`,
+      format: 'cjs',
+    },
+    plugins,
+  };
+}
+
+/**
+ * Generates the Rollup configuration for CJS (CommonJS) builds.
+ *
+ * @param {string} filename - The name of the input TypeScript file.
+ * @returns {ParticularFormatConfig} The Rollup configuration for CJS builds.
+ */
+function getEsmConfig(filename) {
+  return {
+    input: `${dir}${filename}`,
+    output: {
+      dir: './dist',
+      exports: 'default',
+      entryFileNames: `${path.basename(filename, '.ts')}.mjs`,
+      format: 'esm',
+    },
+    plugins,
+  };
+}
+
+/**
+ * Generates the Rollup configuration for CJS (CommonJS) and ESM (ECMAScript Module) builds.
+ * @param {string} filename - The name of the input TypeScript file.
+ * @returns {Array<ParticularFormatConfig>} The Rollup configuration for CJS and ESM builds.
+ */
+function getEntryConfigs(filename) {
+  return [getCjsConfig(filename), getEsmConfig(filename)];
+}
+
+export default files.flatMap(element => getEntryConfigs(element));
