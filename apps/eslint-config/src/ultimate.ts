a55ec616f5d7bb8b0591cd61cc1ef6af298de1af
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-09 18:14:27 +0800

    fix(eslint-config): config error

--- apps/eslint-config/src/ultimate.ts
+++ apps/eslint-config/src/ultimate.ts
@@ -1,8 +1,8 @@
 import merge from 'deepmerge';
 
-import strictCodeStyleConfig from './code-style';
+import { strictCodeStyleConfig } from './code-style';
 import defaultConfig from './default';
-import strictTypescriptConfig from './default';
+import { strictTypescriptConfig } from './typescript-strict';
 
 const config = merge(strictCodeStyleConfig, strictTypescriptConfig);
 
