5623d5eb0ee357453626a30901dfcd9a5372e2b8
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-07-06 19:22:03 +0800

    feat: removed ultimate and code style rules

--- apps/prettier-config/tests/index.test.ts
+++ apps/prettier-config/tests/index.test.ts
@@ -4,9 +4,9 @@ import { describe, expect, it } from 'vitest';
 import config from '../index.json';
 
 describe('Prettier Config', () => {
-  it('is valid configuration', () => {
+  it('is valid configuration', async () => {
     // Format the code with Prettier
-    const code = prettier.format(
+    const code = await prettier.format(
       'const foo=1',
       Object.assign(config as prettier.Options, {
         parser: 'typescript',
