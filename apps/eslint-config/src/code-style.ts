72d2a7b63ffa47e81e408cbd472b2a22d676dac3
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-07-06 19:22:03 +0800

    feat: removed ultimate and code style rules

--- apps/eslint-config/src/code-style.ts
+++
@@ -1,6 +0,0 @@
-import merge from 'deepmerge';
-
-import defaultConfig from './default';
-import strictCodeStyleConfig from './sub-rules/prettier';
-
-export default merge(defaultConfig, strictCodeStyleConfig);
--- apps/eslint-config/src/ultimate.ts
+++
@@ -1,9 +0,0 @@
-import merge from 'deepmerge';
-
-import defaultConfig from './default';
-import strictCodeStyleConfig from './sub-rules/prettier';
-import strictTypescriptConfig from './sub-rules/strict-typescript';
-
-const config = merge(strictCodeStyleConfig, strictTypescriptConfig);
-
-export default merge(defaultConfig, config);
